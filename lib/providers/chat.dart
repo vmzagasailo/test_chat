7b69fe4450900f40cdbe44857dd4f3dafd50a253
Volodymyr <vmzagasailo@gmail.com>
2021-06-07 21:15:18 +0300

    added photos for avatars

--- lib/providers/chat.dart
+++
@@ -1,9 +0,0 @@
-class Chat {
-  final int id;
-  final String name;
-  final imageUrl;
-  final String message;
-  final String date;
-
-  Chat({this.id, this.name, this.message, this.imageUrl, this.date});
-}
