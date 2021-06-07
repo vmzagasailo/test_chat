6dc0f9780fe2588674a33e63d2ac915e27792332
Volodymyr <vmzagasailo@gmail.com>
2021-06-07 18:02:56 +0300

    created chat model

---
+++ lib/providers/chat.dart
@@ -0,0 +1,9 @@
+class Chat {
+  final int id;
+  final String name;
+  final imageUrl;
+  final String message;
+  final String date;
+
+  Chat({this.id, this.name, this.message, this.imageUrl, this.date});
+}
