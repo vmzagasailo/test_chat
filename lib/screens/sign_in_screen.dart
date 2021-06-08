d7584331bd3d3142902709305d163bfcf8db6b7c
Volodymyr <vmzagasailo@gmail.com>
2021-06-08 17:46:23 +0300

    reated a method for login and navigation to the home page

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:test_chat/api/google_signin_api.dart';
+
+import './chat_screen.dart';
+import 'chat_screen.dart';
 
 class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Future signIn() async {
+      final user = await GoogleSignInApi.login();
+      if (user == null) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Sign in Failed')));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => ChatScreen(user: user)));
+            // Navigator.of(context).pushNamed(ChatScreen.routeName, arguments: user);
+      }
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Container(
@@ -26,7 +42,7 @@ class SignInScreen extends StatelessWidget {
                   color: Colors.red,
                 ),
                 label: Text('Sign up with Google'),
-                onPressed: () {},
+                onPressed: signIn,
               ),
             ],
           ),
