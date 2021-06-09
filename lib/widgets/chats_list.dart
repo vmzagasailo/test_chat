6e1271798ab580e6557ce1ac2127b4dea7cda9e1
Volodymyr <vmzagasailo@gmail.com>
2021-06-09 18:30:34 +0300

    search implementation

--- lib/widgets/chats_list.dart
+++ lib/widgets/chats_list.dart
@@ -5,16 +5,69 @@ import '../providers/chats.dart';
 
 import './chat_item.dart';
 
-class ChatsList extends StatelessWidget {
+class ChatsList extends StatefulWidget {
+  @override
+  _ChatsListState createState() => _ChatsListState();
+}
+
+class _ChatsListState extends State<ChatsList> {
+  final _controller = TextEditingController();
+  String _query = '';
+
+  @override
+  void initState() {
+    _controller.addListener(() {
+      setState(() {
+        _query = _controller.text;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   @override
   Widget build(BuildContext context) {
     final chatData = Provider.of<Chats>(context, listen: false);
-    return ListView.builder(
-      itemCount: chatData.messageItemlist.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-        value: chatData.messageItemlist[i],
-        child: ChatItem(),
-      ),
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+          height: 50,
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(32),
+          ),
+          child: TextField(cursorColor: Colors.black,
+            controller: _controller,
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search,color: Colors.black,),
+              hintText: 'search',
+              border: InputBorder.none,
+              
+            ),
+            onChanged: (value) {
+              Provider.of<Chats>(context, listen: false)
+                  .changeSearchString(_query);
+            },
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: chatData.messageItemlist.length,
+            itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+              value: chatData.messageItemlist[i],
+              child: ChatItem(),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
