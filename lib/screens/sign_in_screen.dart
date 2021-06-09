41f930416b2e1acbfb134fe08a67a14bbd9697f4
Volodymyr <vmzagasailo@gmail.com>
2021-06-09 19:14:29 +0300

    error handling

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:test_chat/api/google_signin_api.dart';
-import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import './chat_screen.dart';
@@ -19,14 +18,35 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showErrorDialog(String message) {
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: Text('Something wrong...'),
+          content: Text(message),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+              child: Text('Ok'),
+            )
+          ],
+        ),
+      );
+    }
+
     Future signIn() async {
-      final user = await GoogleSignInApi.login();
-      if (user == null) {
-        ScaffoldMessenger.of(context)
-            .showSnackBar(SnackBar(content: Text('Sign in Failed')));
-      } else {
+      try {
+        final user = await GoogleSignInApi.login();
         Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (context) => ChatScreen(user: user)));
+      } catch (error) {
+        if (error.toString().contains('network_error')) {
+          String errorMessage = 'No internet connection';
+          _showErrorDialog(errorMessage);
+        }
+        throw error;
       }
     }
 
@@ -37,7 +57,7 @@ class SignInScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text(
+              const Text(
                 'Chat',
                 textScaleFactor: 2,
               ),
@@ -53,17 +73,17 @@ class SignInScreen extends StatelessWidget {
                       FontAwesomeIcons.google,
                       color: Colors.red,
                     ),
-                    label: Text('Sign up with Google'),
+                    label: const Text('Sign up with Google'),
                     onPressed: signIn,
                   ),
                   SizedBox(height: 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text('Made by '),
+                      const Text('Made by '),
                       GestureDetector(
                         onTap: _launchUrl,
-                        child: Text(
+                        child: const Text(
                           'Flutter',
                           style: TextStyle(
                             color: Colors.blue,
